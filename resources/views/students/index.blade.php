9c4b217df5999a2935d3620d5dd39b45f3c4f9ff
Fismans <contoh@firmankhanafi12@gmail.com>
2023-09-14 11:09:08 +0700

    initial

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{ url('students/create') }}">Tambah Data</a>
+<table border="1">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>NIS</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php
+        $no = 1;
+        @endphp
+        @foreach ($query as $student)
+        <tr>
+        <td>{{ $no++ }}</td>
+        <td>{{ $student->nis}}</td>
+        <td>{{ $student->name}}</td>
+        <td>{{ $student->class}}</td>
+            <td>
+            <a href="#">Edit</a>
+             <br>
+              <a href="{{ url('students/delete') }}/{{ $student->id}}">Hapus</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+</body>
+</html>
